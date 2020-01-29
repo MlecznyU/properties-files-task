65893bf20d4ac965a1a4bd3c59140387c0eee812
MlecznyU <Tomasz.michlewski@gmail.com>
2020-01-29 19:56:36 +0100

    Added Properties load()

--- src/reader/MyFileReader.java
+++ src/reader/MyFileReader.java
@@ -7,18 +7,33 @@ public class MyFileReader {
 
     public Properties readFileLinesList(String path) {
         //HashMap<String, String> data = new HashMap<>();
+
         Properties data = new Properties();
-        String line;
+
         File file = new File(path);
-        try(BufferedReader br = new BufferedReader(new FileReader(file));) {
-            while ((line=br.readLine()) != null){
-                String[] splited = line.split("=");
-                data.put(splited[0],splited[1]);
-            }
+
+        String line;
+
+
+        try {
+            data.load(new FileReader(file));
         } catch (IOException e) {
             e.printStackTrace();
         }
         data.values().forEach(System.out::println);
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+            while ((line=br.readLine()) != null){
+                String[] splited = line.split("=");
+                data.setProperty(splited[0],splited[1]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        data.values().forEach(System.out::println);
+
         return data;
     }
 }
